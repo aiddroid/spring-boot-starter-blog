eb9304736874d332bf155b8eb9f9c6076faa6167
Allen <aiddroid@gmail.com>
2019-05-06 19:28:50 +0800

    fix:错误页面位置

--- src/main/java/com/example/demo/controller/SiteController.java
+++ src/main/java/com/example/demo/controller/SiteController.java
@@ -7,6 +7,7 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author allen
  */
 @Controller
-@RequestMapping("/site")
-public class SiteController{
+public class SiteController implements ErrorController{
     
     /**
      * 登录界面
@@ -27,11 +27,31 @@ public class SiteController{
      * @param request
      * @return 
      */
-    @RequestMapping(value = "login", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/site/login", method = {RequestMethod.GET, RequestMethod.POST})
     public String login(ModelMap mm, HttpServletRequest request) {
         String message = "hello, world!";
         mm.addAttribute("message", message);
          
         return "/site/login";
+    }
+    
+    /**
+     * 错误页面
+     * @param mm
+     * @param request
+     * @param response
+     * @return 
+     */
+    @RequestMapping("/error")
+    public String error(ModelMap mm, HttpServletRequest request, HttpServletResponse response) {
+        mm.addAttribute("uri", request.getRequestURI());
+        mm.addAttribute("code", response.getStatus());
+        
+        return getErrorPath();
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/site/error";
     }
 }
